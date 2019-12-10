1eab76f0e11d540f16ecb495691eb80ecaf4ba2a
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-12-10 17:18:26 -0500

    models done

--- src/main/java/com/finance/zipBank/Models/Account_Holder.java
+++ src/main/java/com/finance/zipBank/Models/Account_Holder.java
@@ -1,4 +1,84 @@
 package com.finance.zipBank.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+@Entity
 public class Account_Holder {
+
+    @Id
+    private Long account_Id;
+    private String first_Name;
+    private String last_Name;
+    private Integer address_Id;
+    private Integer phone_Number;
+    private String email;
+    private String social_Security;
+    private Date last_Updated;
+
+
+
+    public Long getAccount_Id() {
+        return account_Id;
+    }
+
+    public void setAccount_Id(Long account_Id) {
+        this.account_Id = account_Id;
+    }
+
+    public String getFirst_Name() {
+        return first_Name;
+    }
+
+    public void setFirst_Name(String first_Name) {
+        this.first_Name = first_Name;
+    }
+
+    public String getLast_Name() {
+        return last_Name;
+    }
+
+    public void setLast_Name(String last_Name) {
+        this.last_Name = last_Name;
+    }
+
+    public Integer getAddress_Id() {
+        return address_Id;
+    }
+
+    public void setAddress_Id(Integer address_Id) {
+        this.address_Id = address_Id;
+    }
+
+    public Integer getPhone_Number() {
+        return phone_Number;
+    }
+
+    public void setPhone_Number(Integer phone_Number) {
+        this.phone_Number = phone_Number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSocial_Security() {
+        return social_Security;
+    }
+
+    public void setSocial_Security(String social_Security) {
+        this.social_Security = social_Security;
+    }
+
+    public Date getLast_Updated() {
+        return last_Updated;
+    }
+
+    public void setLast_Updated(Date last_Updated) {
+        this.last_Updated = last_Updated;
+    }
 }
--- src/main/java/com/finance/zipBank/Models/Archived_Transaction_History.java
+++ src/main/java/com/finance/zipBank/Models/Archived_Transaction_History.java
@@ -1,4 +1,73 @@
 package com.finance.zipBank.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Archived_Transaction_History {
+    @Id
+    private Long transaction_Id;
+    private Long account_Id;
+    private Long from_Account;
+    private Long to_Account;
+    private String transaction_Type;
+    private String transaction_Description;
+    private Double amount;
+
+    public Long getTransaction_Id() {
+        return transaction_Id;
+    }
+
+    public void setTransaction_Id(Long transaction_Id) {
+        this.transaction_Id = transaction_Id;
+    }
+
+    public Long getAccount_Id() {
+        return account_Id;
+    }
+
+    public void setAccount_Id(Long account_Id) {
+        this.account_Id = account_Id;
+    }
+
+    public Long getFrom_Account() {
+        return from_Account;
+    }
+
+    public void setFrom_Account(Long from_Account) {
+        this.from_Account = from_Account;
+    }
+
+    public Long getTo_Account() {
+        return to_Account;
+    }
+
+    public void setTo_Account(Long to_Account) {
+        this.to_Account = to_Account;
+    }
+
+    public String getTransaction_Type() {
+        return transaction_Type;
+    }
+
+    public void setTransaction_Type(String transaction_Type) {
+        this.transaction_Type = transaction_Type;
+    }
+
+    public String getTransaction_Description() {
+        return transaction_Description;
+    }
+
+    public void setTransaction_Description(String transaction_Description) {
+        this.transaction_Description = transaction_Description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
