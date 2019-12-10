2edf1cfd7fc2b9c964580c53ef6cbed70bb517c4
ushanil <ush.gopi@gmail.com>
2019-12-10 14:03:56 -0500

    Iniital setup

---
+++ src/main/java/com/finance/zipBank/ZipBankApplication.java
@@ -0,0 +1,13 @@
+package com.finance.zipBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance/zipBank/ZipBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.zipBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
