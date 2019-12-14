f733f46c041360226001eef00f74d811cc1412cc
chung <chung305>
2019-12-14 12:41:05 -0500

    added some web security

---
+++ src/main/java/com/finance/zipBank/WebSecurityConfigurations/WebSecurityConfigs.java
@@ -0,0 +1,44 @@
+package com.finance.zipBank.WebSecurityConfigurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigs  extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //allows
+        http.authorizeRequests()
+                .antMatchers("/", "path that will not need authentication").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    //implement user ids to check authentications
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(" ")
+                .password(" ")
+                .roles("User")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
