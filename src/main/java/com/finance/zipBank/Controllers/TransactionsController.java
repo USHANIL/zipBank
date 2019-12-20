0ed4693e279c3a98e27b1ccf981ed861e925c8c2
Julia Evans <julevans93@users.noreply.github.com>
2019-12-20 16:39:27 -0500

    working on transactions controller

--- src/main/java/com/finance/zipBank/Controllers/TransactionsController.java
+++ src/main/java/com/finance/zipBank/Controllers/TransactionsController.java
@@ -1,11 +1,15 @@
 package com.finance.zipBank.Controllers;
 
+
 import com.finance.zipBank.Models.Transactions;
 import com.finance.zipBank.Service.TransactionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransactionsController {
