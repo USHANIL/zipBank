8b6d0dfcdbf4687aed4c6c3dff2828c404f8c1ac
Julia Evans <julevans93@users.noreply.github.com>
2019-12-20 15:56:35 -0500

    working on transaction controller and service

--- src/main/java/com/finance/zipBank/Controllers/TransactionsController.java
+++ src/main/java/com/finance/zipBank/Controllers/TransactionsController.java
@@ -1,12 +1,27 @@
 package com.finance.zipBank.Controllers;
 
+import com.finance.zipBank.Models.Transactions;
 import com.finance.zipBank.Service.TransactionsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TransactionsController {
 
     @Autowired
     private TransactionsService transactionsService;
+
+    @PostMapping("user/{id}/accounts/{id}")
+    public ResponseEntity<Transactions> create(@PathVariable Long id){
+        return new ResponseEntity<>(transactionsService.create(id), HttpStatus.CREATED);
+    }
+
+    //list all transactions
+    @GetMapping("/accounts/{accountNumber}")
+    public  ResponseEntity<Iterable<Transactions>> listAllTransactions(@PathVariable Long accountNumber){
+        return new ResponseEntity<>(transactionsService.list(accountNumber), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/finance/zipBank/Service/TransactionsService.java
+++ src/main/java/com/finance/zipBank/Service/TransactionsService.java
@@ -1,12 +1,46 @@
 package com.finance.zipBank.Service;
 
+import com.finance.zipBank.Models.Accounts;
+import com.finance.zipBank.Models.Transactions;
+import com.finance.zipBank.Repositories.AccountsRepo;
 import com.finance.zipBank.Repositories.TransactionsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TransactionsService {
 
     @Autowired
     private TransactionsRepo transactionsRepo;
+
+    @Autowired
+    private AccountsRepo accountsRepo;
+
+    //create transaction
+    public Transactions create(Long accountNumber){
+        Accounts accounts = accountsRepo.findById(accountNumber).get();
+
+        Transactions transaction = new Transactions();
+        transaction.setAccountNumber(accounts.getAccountNumber());
+        transaction.setAmount(0L);
+        transaction.setType("");
+
+        return transactionsRepo.save(transaction);
+    }
+
+    //list all transactions for specific account
+    public List<Transactions> list(Long accountNumber) {
+        Iterable<Transactions> listTransactions = transactionsRepo.findAll();
+        List<Transactions> transactionList = new ArrayList<>();
+        for (Transactions transactions : listTransactions){
+            if (transactions.getAccountNumber().equals(accountNumber)) {
+                transactionList.add(transactions);
+            }
+        }
+        return transactionList;
+    }
+
 }
