3296f086e0dc90b902e7a8621b8b56f2aa60ab4d
Julia Evans <julevans93@users.noreply.github.com>
2019-12-20 18:05:12 -0500

    in progress

--- src/main/java/com/finance/zipBank/Repositories/TransactionsRepo.java
+++ src/main/java/com/finance/zipBank/Repositories/TransactionsRepo.java
@@ -4,6 +4,9 @@ import com.finance.zipBank.Models.Transactions;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionsRepo extends CrudRepository<Transactions, Long> {
+    List<Transactions> findAllByAccountNumber (Long accountNumber);
 }
--- src/main/java/com/finance/zipBank/Service/TransactionsService.java
+++ src/main/java/com/finance/zipBank/Service/TransactionsService.java
@@ -33,14 +33,14 @@ public class TransactionsService {
 
     //list all transactions for specific account
     public List<Transactions> list(Long accountNumber) {
-        Iterable<Transactions> listTransactions = transactionsRepo.findAll();
-        List<Transactions> transactionList = new ArrayList<>();
-        for (Transactions transactions : listTransactions){
-            if (transactions.getAccountNumber().equals(accountNumber)) {
-                transactionList.add(transactions);
-            }
-        }
-        return transactionList;
+//        Iterable<Transactions> listTransactions = transactionsRepo.findAll();
+//        List<Transactions> transactionList = new ArrayList<>();
+//        for (Transactions transactions : listTransactions){
+//            if (transactions.getAccountNumber().equals(accountNumber)) {
+//                transactionList.add(transactions);
+//            }
+//        }
+        return transactionsRepo.findAllByAccountNumber(accountNumber);
     }
 
 }
