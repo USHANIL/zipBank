939225ca899a14f075788d03398d1152aba48fc7
chung <chung305>
2020-01-01 16:57:38 -0500

    set last login on authentication

--- src/main/java/com/finance/zipBank/Configurations/SecurityConfiguration.java
+++ src/main/java/com/finance/zipBank/Configurations/SecurityConfiguration.java
@@ -37,6 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers( "/authenticate", "/register").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/com/finance/zipBank/Controllers/LoginController.java
+++ src/main/java/com/finance/zipBank/Controllers/LoginController.java
@@ -1,7 +1,9 @@
 package com.finance.zipBank.Controllers;
 
+import com.finance.zipBank.Models.User;
 import com.finance.zipBank.Models.authentication.AuthenticationRequest;
 import com.finance.zipBank.Models.authentication.AuthenticationResponse;
+import com.finance.zipBank.Repositories.UserRepo;
 import com.finance.zipBank.Service.userDetails.MyUserDetailsService;
 import com.finance.zipBank.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class LoginController {
 
@@ -26,6 +30,9 @@ public class LoginController {
 
     @Autowired
     private JwtUtil jwtTokenUtil;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/authenticate")
@@ -41,6 +48,11 @@ public class LoginController {
                 .loadUserByUsername(authenticationRequest.getUsername());
 
         final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        //setting user last login
+        User user = userRepo.findByUsername(authenticationRequest.getUsername());
+        user.setLastLogin(new Date());
+        userRepo.save(user);
 
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
