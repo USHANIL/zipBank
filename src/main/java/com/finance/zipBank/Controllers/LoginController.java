527ff4015c6327683fb60a0d2e56c910375bbc93
BrianP <bpattjr@gmail.com>
2020-01-03 17:36:21 -0500

    merging changes

--- src/main/java/com/finance/zipBank/Controllers/LoginController.java
+++ src/main/java/com/finance/zipBank/Controllers/LoginController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 
     @Autowired
--- src/main/java/com/finance/zipBank/Controllers/UserController.java
+++ src/main/java/com/finance/zipBank/Controllers/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
