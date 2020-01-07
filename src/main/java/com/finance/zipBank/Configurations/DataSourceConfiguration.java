fe35183b598958d00518fe465b17aa4697bc8e66
ushanil <ush.gopi@gmail.com>
2020-01-06 19:16:47 -0500

    datasourceconfi

--- src/main/java/com/finance/zipBank/Configurations/DataSourceConfiguration.java
+++ src/main/java/com/finance/zipBank/Configurations/DataSourceConfiguration.java
@@ -2,6 +2,7 @@ package com.finance.zipBank.Configurations;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,9 +11,9 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 
 @Configuration
-@Profile("cloud")
+//@Profile("cloud")
 public class DataSourceConfiguration {
-
+/*
     private static final String USER_NAME = System.getenv("DB_UN");
     private static final String PASSWORD = System.getenv("DB_PW");
     private static final String URL = System.getenv("DB_URL");
@@ -27,5 +28,16 @@ public class DataSourceConfiguration {
         hikariConfig.setDriverClassName("org.postgresql.Driver");
 
         return new HikariDataSource(hikariConfig);
+    }
+
+ */
+@Value("${spring.datasource.url}")
+private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
     }
 }
