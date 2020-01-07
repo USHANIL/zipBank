6e33dfb8d0e1a725fe496e21917769422489861d
ushanil <ush.gopi@gmail.com>
2020-01-07 11:42:05 -0500

    springbootservletinitializer

--- src/main/java/com/finance/zipBank/ZipBankApplication.java
+++ src/main/java/com/finance/zipBank/ZipBankApplication.java
@@ -3,9 +3,10 @@ package com.finance.zipBank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication()
-public class ZipBankApplication {
+public class ZipBankApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipBankApplication.class, args);
