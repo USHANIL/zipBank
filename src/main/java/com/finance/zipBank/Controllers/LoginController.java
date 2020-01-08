bb3c9d76541ec81db0cc9f85b8e84e2fd3f86a77
ushanil <ush.gopi@gmail.com>
2020-01-08 14:09:57 -0500

    localhost for crossorigin

--- src/main/java/com/finance/zipBank/Controllers/LoginController.java
+++ src/main/java/com/finance/zipBank/Controllers/LoginController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
-@CrossOrigin(origins = "http://zipbank.herokuapp.com")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
 
     @Autowired
@@ -35,7 +35,7 @@ public class LoginController {
     @Autowired
     private UserRepo userRepo;
 
-    @CrossOrigin(origins = "http://zipbank.herokuapp.com")
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthentication(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
         try {
